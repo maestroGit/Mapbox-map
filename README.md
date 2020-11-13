b75423498b126ab7bc226d0e8a58f2bc9ea46bc4
walkexperience <walkexpereince@gmail.com>
2020-11-13 22:20:46 +0100

    Add direction plugin & markers

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
+# MAPBOX
 🗺️ Mapbox GL JS is a JavaScript library that uses WebGL to render interactive maps from vector tiles and Mapbox styles. 
 https://docs.mapbox.com/
 
+Display driving directions
+https://docs.mapbox.com/mapbox-gl-js/example/mapbox-gl-directions/
+
+## Construido con 🛠️
 🧰 Toolbox: installed Mapbox GL JS: Including it with a <link> in the head of the document via the Mapbox CDN.
+
+### Notas 📋
+Pediente añadir menú superior que active las funcionalidad de rutas
+
+* Las mejoras o comentarios son bienvenidos 📢
+⌨️ con ☕ por [maestroGit](https://github.com/maestroGit) 😊
--- js/script.js
+++ js/script.js
@@ -1,32 +1,73 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoid2Fsa2V4cGVyaWVuY2UiLCJhIjoiY2toZXo4Njg0MGt3NTJ5bnYwam1za3gybiJ9.likY50yxjntRWPEvwuYDDQ";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoid2Fsa2V4cGVyaWVuY2UiLCJhIjoiY2toZXo4Njg0MGt3NTJ5bnYwam1za3gybiJ9.likY50yxjntRWPEvwuYDDQ';
-
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-});
+// var map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+//   center: [-74.5, 40], // starting position [lng, lat]
+//   zoom: 9, // starting zoom
+// });
 
 const locationDevice = navigator.geolocation; //Returns a Geolocation object allowing accessing the location of the device.
 console.log(locationDevice);
 
 function getLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(scCallbackCoords);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
   }
-  
-  function showPosition(position) {
-    //console.log(position);
-    console.log( "Latitude: " + position.coords.latitude +
-    " Longitude: " + position.coords.longitude)
-  }
+}
 
-  getLocation();
+function scCallbackCoords(position) {
+  console.log(typeof position);
+  let lat = position.coords.latitude;
+  let lng = position.coords.longitude;
+  console.log(lat, lng);
+  centreMap([lng, lat]);
+}
 
+function centreMap(center) {
+  //map object
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+    center: center, // Paso array [lng, lat] (primero longitud)
+    zoom: 1, // starting zoom
+  });
 
-   const nav = new mapboxgl.NavigationControl()
-   map.addControl(nav)
+  // zoom control nav
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  // Display Pop up
+  var popup = new mapboxgl.Popup({ closeOnClick: false })
+    .setLngLat(center)
+    .setHTML("<h1>Hello World!</h1>")
+    .addTo(map);
+
+  // create the popup
+  var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+    "Construction on the Washington Monument began in 1848."
+  );
+
+  // create DOM element for the marker
+  var el = document.createElement("div");
+  el.id = "marker";
+
+  // create the marker
+  var monument = [-77.0353, 38.8895];
+  new mapboxgl.Marker(el)
+    .setLngLat(monument)
+    .setPopup(popup) // sets a popup on this marker
+    .addTo(map);
+
+  // Directions plugins
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, "top-left");
+}
+
+getLocation();
